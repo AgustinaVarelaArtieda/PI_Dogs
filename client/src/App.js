2bb9296d9ed7a9df42189f634b69e57a4258c0e8
agusv <agusvarela5@gmail.com>
2023-07-30 20:33:14 -0300

    Problema con carpeta client. Ya cambiada y arreglado.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Routes>
+        <Route path='/' element={<h1>Landing</h1>}/>
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/home/:id" element={<h1>Detail</h1>} />
+        <Route path="/newdog" element={<h1>NewDog</h1>} />
+        <Route path="*" element={<h1>Error</h1>} />
+      </Routes>
     </div>
   );
 }
