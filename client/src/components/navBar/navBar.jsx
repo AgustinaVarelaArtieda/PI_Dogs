360c367a905bde859bb435ace5e40dd4d80ca9b3
agusv <agusvarela5@gmail.com>
2023-08-01 23:10:17 -0300

    Se agregaron los enlaces de navegacion. Falta aplicar estilos.

---
+++ client/src/components/navBar/navBar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+import style from './navBar.module.css'
+
+export default function NavBar(){
+    return(
+        <div className={style.navBar}>
+            <NavLink to='/home' className={style.link}>Home</NavLink>
+            <NavLink to='/about' className={style.link}>About</NavLink>
+            <NavLink to='/newdog' className={style.link}>New Dog</NavLink>
+        </div>
+    )
+}
