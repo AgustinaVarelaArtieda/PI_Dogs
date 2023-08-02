e3e0554daf81cee3d6770badaf6552523a0590c2
agusv <agusvarela5@gmail.com>
2023-08-01 23:13:28 -0300

    Se agrego correctamente el link al detail.

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import { Link } from 'react-router-dom'; 
 
 import style from './card.module.css';
 
-export default function Card({name, image, temperament, weight_min, weight_max}){
+export default function Card({id, name, image, temperament, weight_min, weight_max}){
+    // const navigate=useNavigate();
+
+    // function handleClick(e){
+    //     e.preventDefault();
+    //     navigate(`/detail/${id}`)
+    // }
+    
     return(
         <div className={style.card2}> 
             <h1>{name}</h1>
-            <img src={image} alt={name}/>
+
+            <Link to={`/detail/${id}`}>
+                <img src={image} alt={name}/>
+            </Link>
+            
             <p>{temperament}</p>
-            <p>{weight_min} - {weight_max}</p>
+
+            {isNaN(weight_min)&&isNaN(weight_max) ? (
+                <h3>no data</h3> 
+            ) : (
+            isNaN(weight_min) ? (
+                <h3>no data - {weight_max} kg</h3>
+            ) : (
+            isNaN(weight_max) ? (
+                <h3>{weight_min} kg - no data</h3>
+            ) : (
+                <h3>{weight_min} kg - {weight_max} kg</h3>
+            )
+            ))}
         </div>
     )
 }
