c735aab943ec72de4a42a44e4830dc84873b6def
agusv <agusvarela5@gmail.com>
2023-08-01 23:08:32 -0300

    SearchBar funcional. Falta css.

---
+++ client/src/components/searchBar/searchBar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { searchDogName } from '../../redux/actions';
+
+import style from './searchBar.module.css'
+
+export default function SearchBar(){
+    const dispatch=useDispatch()
+
+    //creo un ESTADO LOCAL
+    const [name,setName]=useState()
+
+    //En esta funcion guardo en mi estado local lo que se agregue en mi input
+    function handleInputChange(e){
+        e.preventDefault()
+        setName(e.target.value)
+    }
+
+    //En esta le doy funcionalidad al boton
+    function handleSubmit(e){
+        e.preventDefault()
+        dispatch(searchDogName(name))
+
+        //Para limpiar el input
+        setName('')
+    }
+
+    return(
+        <div className={style.group}>
+            <input type="text" placeholder='Buscar...' onChange={(e)=>handleInputChange(e)}></input>
+            <button className={style.btnS} type='submit' onClick={(e)=>handleSubmit(e)}>🔍︎</button>
+        </div>
+    )
+}
