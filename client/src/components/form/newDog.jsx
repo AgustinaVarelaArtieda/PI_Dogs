72e6f77a4ac99e01c939b4e0bf78bb0a5ef6e126
agusv <agusvarela5@gmail.com>
2023-08-03 23:05:54 -0300

    Formulario funcional, con validaciones hechas. Falta css.

--- client/src/components/form/newDog.jsx
+++ client/src/components/form/newDog.jsx
@@ -13,13 +13,16 @@ export default function FormPage(){
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    //Imagen para cuando no ingresan imagen
+    const imgDefault='https://thumbs.gfycat.com/DependableElementaryAquaticleech-size_restricted.gif'
+
     //Traigo los temperamentos del estado Global
     const allTemperaments = useSelector((state) => state.dogTemps); 
 
     //Creo un estado Local para los inputs del form
     const [newDog, setNewDog] = useState({
         name: '',
-        image:'',
+        image:imgDefault,
         life_span:'',
         weight_min: '',
         weight_max: '',
@@ -44,6 +47,7 @@ export default function FormPage(){
             [e.target.name]: e.target.value
         });
 
+        console.log(errors)
         //Para verificar las validaciones
         setErrors(validate({
             ...newDog,
@@ -66,6 +70,10 @@ export default function FormPage(){
               ...newDog,
               temperament: [...newDog.temperament, selectedTemperament]
             });
+            setErrors({
+                ...errors,
+                temperament:null
+            });
           }
     };
 
@@ -78,27 +86,31 @@ export default function FormPage(){
         });
     };
 
+    
     //Para limpiar el input y cargar la info
     function handleSubmit(e){
         e.preventDefault();
         
-        dispatch(postDog(newDog));
+        if(!errors.name&&!errors.life_span&&!errors.weight_min&&!errors.weight_max&&!errors.height_max&&!errors.height_min&&!errors.temperament){
+            dispatch(postDog(newDog));
+    
+            alert('Dog created successfully!');
+    
+            setNewDog({
+                name: '',
+                image:'',
+                life_span:'',
+                weight_min: '',
+                weight_max: '',
+                height_min: '',
+                height_max: '',
+                temperament: []
+            });
+    
+            //Me redirige a home
+            navigate('/home');
 
-        alert('Dog created successfully!')
-
-        setNewDog({
-            name: '',
-            image:'',
-            life_span:'',
-            weight_min: '',
-            weight_max: '',
-            height_min: '',
-            height_max: '',
-            temperament: []
-        });
-
-        //Me redirige a home
-        navigate('/home');
+        } else alert('There is incomplete data');
     }
 
     return(
@@ -110,49 +122,49 @@ export default function FormPage(){
             <form className={styles.form} onSubmit={(e)=>handleSubmit(e)}>
                 <div>
                     <label>Name</label>
-                    <input type="text" value={newDog.name} name='name' onChange={(e)=>handleChange(e)}></input>
+                    <input type="text" value={newDog.name} name='name' placeholder='Enter name...' onChange={(e)=>handleChange(e)}></input>
                     {errors.name && (
                         <span className="error">{errors.name}</span>
                     )}
                 </div>
                 <div>
                     <label>Image</label>
-                    <input type="text" value={newDog.image} name='image' onChange={(e)=>handleChange(e)}></input>
+                    <input type="text" value={newDog.image===imgDefault?'':newDog.image} name='image' placeholder='Enter url image...' onChange={(e)=>handleChange(e)}></input>
                     {errors.image && (
                         <span className="error">{errors.image}</span>
                     )}
                 </div>
                 <div>
                     <label>Life Span</label>
-                    <input type="text" value={newDog.life_span} name='life_span' onChange={(e)=>handleChange(e)}></input>
+                    <input type="text" value={newDog.life_span} name='life_span' placeholder='X - X years' onChange={(e)=>handleChange(e)}></input>
                     {errors.life_span && (
                         <span className="error">{errors.life_span}</span>
                     )}  
                 </div>
                 <div>
                     <label>Weight Min</label>
-                    <input type="text" value={newDog.weight_min} name='weight_min' onChange={(e)=>handleChange(e)}></input>
+                    <input type="number" value={newDog.weight_min} name='weight_min' placeholder=' - ' onChange={(e)=>handleChange(e)}></input>
                     {errors.weight_min && (
                         <span className="error">{errors.weight_min}</span>
                     )}  
                 </div>
                 <div>
                     <label>Weight Max</label>
-                    <input type="text" value={newDog.weight_max} name='weight_max' onChange={(e)=>handleChange(e)}></input>
+                    <input type="number" value={newDog.weight_max} name='weight_max' placeholder=' - ' onChange={(e)=>handleChange(e)}></input>
                     {errors.weight_max && (
                         <span className="error">{errors.weight_max}</span>
                     )}  
                 </div>
                 <div>
                     <label>Height Min</label>
-                    <input type="text" value={newDog.height_min} name='height_min' onChange={(e)=>handleChange(e)}></input>
+                    <input type="number" value={newDog.height_min} name='height_min' placeholder=' - ' onChange={(e)=>handleChange(e)}></input>
                     {errors.height_min && (
                         <span className="error">{errors.height_min}</span>
                     )}  
                 </div>
                 <div>
                     <label>Height Max</label>
-                    <input type="text" value={newDog.height_max} name='height_max' onChange={(e)=>handleChange(e)}></input>
+                    <input type="number" value={newDog.height_max} name='height_max' placeholder=' - ' onChange={(e)=>handleChange(e)}></input>
                     {errors.height_max && (
                         <span className="error">{errors.height_max}</span>
                     )}
@@ -160,13 +172,14 @@ export default function FormPage(){
                 <div>
                     <label>Temperaments</label>
                     <select onChange={(e) => handleTemperament(e)} >
-                        <option value="">Seleccione un temperamento</option>
+                        <option value="" disabled>Select temperaments</option>
                             {allTemperaments.map((temperament) => (
                                 <option key={temperament} value={temperament}>
                                     {temperament}
                                 </option>
                             ))}
                     </select>
+                            
                     {newDog.temperament.length > 0 && (
                      <div>
                         <p>Selected temperaments: </p>
@@ -175,6 +188,9 @@ export default function FormPage(){
                         ))}
                      </div>
                     )}
+                    {errors.temperament && (
+                                <span className="error">{errors.temperament}</span>
+                            )}
                 </div>
 
                 <button type="submit">Create Dog!</button>
--- client/src/components/form/validates.js
+++ client/src/components/form/validates.js
@@ -1,6 +1,64 @@
-export default function validate(input){
+export default function validate(newDog){
     let errors={}
-    if(!input.name){
-        errors.name="Name is required"
-    }
+
+    //Validaciones regex
+    const numEnteroPositivo=/^[1-9]\d*$/;
+    const noNumeros=/^[^\d]*$/;
+
+    //Errores en nombre
+    if(!newDog.name){
+        errors.name="Name is required."
+    }else if(!noNumeros.test(newDog.name)){
+        errors.name="The name must not contain numbers."
+    }else errors.name=null
+
+    //Errores en weight
+    if(!newDog.weight_min){
+        errors.weight_min="Weight min is required."
+    }else if(!numEnteroPositivo.test(newDog.weight_min)){
+        errors.weight_min="Weight min must be a number."
+    }else errors.weight_min=null
+
+    if(!newDog.weight_max){
+        errors.weight_max="Weight max is required."
+    }else if(!numEnteroPositivo.test(newDog.weight_max)){
+        errors.weight_max="Weight max must be a number."
+    }else if(newDog.weight_max<newDog.weight_min){
+        errors.weight_max="Weight max must be greater than the Weight min."
+    }else if(newDog.weight_max>110){
+        errors.weight_max="Weight max must be less than 110kgs."
+    }else errors.weight_max=null
+
+    //Errores en height
+    if(!newDog.height_min){
+        errors.height_min="Height min is required."
+    }else if(!numEnteroPositivo.test(newDog.height_min)){
+        errors.height_min="Weight min must be a number."
+    }else errors.height_min=null
+
+    if(!newDog.height_max){
+        errors.height_max="Height max is required."
+    }else if(!numEnteroPositivo.test(newDog.height_max)){
+        errors.height_max="Weight min must be a number."
+    }else if(newDog.height_max<newDog.height_min){
+        errors.height_max="Height max must be greater than the Height min."
+    }else if(newDog.height_max>120){
+        errors.height_max="Height max must be less than 120cm."
+    }else errors.height_max=null
+
+    //Errores en life_span
+    if(!newDog.life_span){
+        errors.life_span="Life span is required"
+    }else if(!numEnteroPositivo.test(newDog.life_span)){
+        errors.life_span="Weight min must be a number."
+    }else if(newDog.life_span>20){
+        errors.life_span="Dogs have an average lifespan of 20 years."
+    }else errors.life_span=null
+
+    //Errores en temperamentos
+    if(newDog.temperament.length===0){
+        errors.temperament="At least one temperament is required."
+    }else errors.temperament=null
+
+    return errors
 }
