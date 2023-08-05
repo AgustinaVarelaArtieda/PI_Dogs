86098a051b1268104a807729a0d31d6e9b623662
agusv <agusvarela5@gmail.com>
2023-08-04 23:13:35 -0300

    Cards terminadas. Listo para empezar con css.

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import style from './card.module.css';
 
 export default function Card({id, name, image, temperament, weight_min, weight_max}){
-    // const navigate=useNavigate();
-
-    // function handleClick(e){
-    //     e.preventDefault();
-    //     navigate(`/detail/${id}`)
-    // }
+    let temps=''
     
+    if(typeof temperament === 'string'){
+        temps= temperament
+    }
+    if(Array.isArray(temperament)){
+        temps=temperament.map(el=>el.name).join(', ')                                      
+    }
+
     return(
         <div className={style.card2}> 
             <h1>{name}</h1>
@@ -18,7 +20,7 @@ export default function Card({id, name, image, temperament, weight_min, weight_m
                 <img src={image} alt={name}/>
             </Link>
             
-            <p>{temperament}</p>
+            <p>{temps}</p>
 
             {isNaN(weight_min)&&isNaN(weight_max) ? (
                 <h3>no data</h3> 
