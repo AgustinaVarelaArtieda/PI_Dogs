c672b78c72327061fec0a555b150a9c6ff8e01ed
agusv <agusvarela5@gmail.com>
2023-08-08 01:17:48 -0300

    Css basico.

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'; 
+import { NavLink } from 'react-router-dom'; 
 
 import style from './card.module.css';
 
@@ -13,27 +13,30 @@ export default function Card({id, name, image, temperament, weight_min, weight_m
     }
 
     return(
-        <div className={style.card2}> 
-            <h1>{name}</h1>
+      <div className={style.card}> 
+      <div className={style.card2}>
+     <NavLink to={`/detail/${id}`} style={{textDecoration:'none'}}>
+        <img src={image} width="300px" height="200px" alt={name}/>
+        <h1>{name}</h1>
+        
+        
+          <p>{temps?temps:'No data about temperaments'}</p>
+        
 
-            <Link to={`/detail/${id}`}>
-                <img src={image} alt={name}/>
-            </Link>
-            
-            <p>{temps}</p>
-
-            {isNaN(weight_min)&&isNaN(weight_max) ? (
-                <h3>no data</h3> 
-            ) : (
+        {isNaN(weight_min)&&isNaN(weight_max) ? (
+          <h3>no data</h3> 
+          ) : (
             isNaN(weight_min) ? (
-                <h3>no data - {weight_max} kg</h3>
+              <h3>no data - {weight_max} kg</h3>
             ) : (
-            isNaN(weight_max) ? (
+              isNaN(weight_max) ? (
                 <h3>{weight_min} kg - no data</h3>
-            ) : (
+              ) : (
                 <h3>{weight_min} kg - {weight_max} kg</h3>
-            )
-            ))}
-        </div>
+          )))}
+        
+     </NavLink>
+      </div>
+      </div>
     )
 }
