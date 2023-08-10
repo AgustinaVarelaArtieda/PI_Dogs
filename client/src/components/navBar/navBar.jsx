c0f6ce80130b74489d38602fc8a183a67a562670
agusv <agusvarela5@gmail.com>
2023-08-09 23:19:10 -0300

    Estilos terminados.

--- client/src/components/navBar/navBar.jsx
+++ client/src/components/navBar/navBar.jsx
@@ -5,9 +5,9 @@ import style from './navBar.module.css'
 export default function NavBar(){
     return(
         <div className={style.navBar}>
-            <NavLink to='/home' className={style.link}>Home</NavLink>
-            <NavLink to='/about' className={style.link}>About</NavLink>
-            <NavLink to='/newdog' className={style.link}>New Dog</NavLink>
+            <NavLink to='/home' className={style.link}>+ Home</NavLink>
+            <NavLink to='/about' className={style.link}>+ About</NavLink>
+            <NavLink to='/newdog' className={style.link}>+ New Dog </NavLink>
         </div>
     )
 }
