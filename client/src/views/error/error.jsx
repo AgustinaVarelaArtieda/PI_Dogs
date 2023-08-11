f08d8952139007f6218c9389bca18bc50a4ca775
agusv <agusvarela5@gmail.com>
2023-08-11 19:12:06 -0300

    Css terminado.

--- client/src/views/error/error.jsx
+++ client/src/views/error/error.jsx
@@ -1,13 +1,17 @@
-import React from "react";
 import {Link} from 'react-router-dom'
 
-export default function ErrorPage(){
-    return(
-        <div>
-            <h1>Error 404: Page not found</h1>
+import style from './error.module.css'
 
+export default function ErrorPage(){
+    
+    return(
+        <div className={style.layout}>
+            <div className={style.contend}>
+                <h1>* Error *</h1>
+                <h2>Page not found</h2>
+            </div>
             <Link to='/home'>
-                <button>HOME</button>
+                <button> Back to home</button>
             </Link>
         </div>
     )
