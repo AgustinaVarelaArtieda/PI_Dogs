ccc9042dd7ec3fc5345a6da7479686e84b12ba80
agusv <agusvarela5@gmail.com>
2023-08-11 21:33:36 -0300

    Errores en consola corregidos.

--- client/src/views/about/about.jsx
+++ client/src/views/about/about.jsx
@@ -24,17 +24,19 @@ export default function AboutPage(){
                     </div>
                 </div>
                 <div className={style.info2}>
-                    <text>Hacer este proyecto de crear una pagina web fue un proceso con altibajos, pero al ver a mi peludo Milo todos los dias me recordaba que tenia que volver a la carga con la pagina.
+                    <span>Hacer este proyecto de crear una pagina web fue un proceso con altibajos, pero al ver a mi peludo Milo todos los dias me recordaba que tenia que volver a la carga con la pagina.
                         <br/>
                         Al realizarla pude reafirmar y volcar lo aprendido en el curso de Henry, mejorar mis practicas de codeo y, especialmente, aprender a solucionar bugs! 
-                    </text>
+                    </span>
+                    <div className={style.bot}>
                     <h2>Sobre mi:</h2>
-                    <text>Paso mi tiempo libre leyendo, viendo anime o jugando en la grieta del invocador. Mi autor favorito es Brandon Sanderson!
+                    <span>Paso mi tiempo libre leyendo, viendo anime o jugando en la grieta del invocador. Mi autor favorito es Brandon Sanderson!
                         <br/>
                         Una de mis aspiraciones en este nuevo camino de programacion que he elegido es llegar a trabajar en un videojuego o en la empresa de RIOT para sacar skins legendarias a mis campeones favoritos (Tranquilo Malza, tu seras el primero).
                         <br/>
                         <p>{'{'}</p>
-                    </text>
+                    </span>
+                    </div>
                 </div>
             </div>
             </div>
